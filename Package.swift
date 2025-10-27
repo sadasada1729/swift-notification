f7f2b6bd3245bffc537bb09d28c19938a7a6d5bc
osada <osadab6mb@gmail.com>
2025-10-27 16:01:10 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NotificationCore",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "NotificationCore",
+            targets: ["NotificationCore"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "NotificationCore"
+        ),
+        .testTarget(
+            name: "NotificationCoreTests",
+            dependencies: ["NotificationCore"]
+        ),
+    ]
+)
